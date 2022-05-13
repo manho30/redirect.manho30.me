e32afc1366476d8d8931e650ab444283b70db308
manho30 <manhochan30@gmail.com>
2022-05-13 22:10:09 +0800

    fix non http link redirect with unsucces

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-
 if (!$_GET['r']) {
     http_response_code(400);
     header("Content-type: application/json");
     echo json_encode(array("error" => "No query string provided"));
 } else {
-    header("location: ".$_GET['r']);
+    // check is the query string include http and make a redirect
+    if (strpos($_GET['r'], "http") === false) {
+        $url = "http://" . $_GET['r'];
+        header("Location: " . $url);
+    } else {
+        $url = $_GET['r'];
+        header("Location: " . $url);
+    }
+    
+    header();
 }
 ?>
